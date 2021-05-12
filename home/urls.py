188e44e2657fe8c55e545189f5aa139901cb900d
Park Donghyeon <emma415g@gmail.com>
2021-05-13 04:15:45 +0900

    front-end 내부 호출 api 변경
    * back-end api 부분 수정
    * 조건식 페이지 추가
    * 개별종목 페이지 추가

--- home/urls.py
+++ home/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'home'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('filter/<str:filter_id>', views.filter_detail, name='filter_detail'),
-    path('stock/<str:stock_code>', views.stock_detail, name='stock_detail'),
-    path('favorite/<str:favorite_id>', views.favorite_detail, name='favorite_detail')
+    path('filter/', views.filter_detail, name='filter_detail'),
+    path('stock/', views.stock_detail, name='stock_detail'),
+    path('favorite/', views.favorite_detail, name='favorite_detail')
 ]
--- home/views.py
+++ home/views.py
@@ -5,13 +5,13 @@ def index(request):
     return render(request, 'home/index.html')
 
 
-def filter_detail(request, filter_id):
+def filter_detail(request):
     return render(request, 'home/filter_detail.html')
 
 
-def stock_detail(request, stock_code):
+def stock_detail(request):
     return render(request, 'home/stock_detail.html')
 
 
-def favorite_detail(request, favorite_id):
+def favorite_detail(request):
     return render(request, 'home/filter_detail.html')
