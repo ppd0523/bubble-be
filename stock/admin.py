df8b1a5d6f2ccfbe9124380c4a9aafc6d99f11f8
Park Donghyeon <emma415g@gmail.com>
2021-08-10 06:06:39 +0900

    중복DB 삽입 못하게 하는 기능 추가할 것! 관리자 모드에서 검색필드 추가
    * price는 검색 안되는 버그 수정
    * 날짜 검색 가능

--- stock/admin.py
+++ stock/admin.py
@@ -7,13 +7,19 @@ class FilterAdmin(admin.ModelAdmin):
 
 
 class ReportAdmin(admin.ModelAdmin):
+    date_hierarchy = 'create_date'
     search_fields = ['filter_id', 'stock_name', 'create_date']
 
 
 class PriceAdmin(admin.ModelAdmin):
-    search_fields = ['stock_name']
+    # search_fields = ['stock_name']
+    date_hierarchy = 'create_date'
+    fieldsets = [
+        (None, {'fields': ['stock_name']}),
+        (None, {'fields': ['create_date']}),
+    ]
 
 
 admin.site.register(Filter, FilterAdmin)
 admin.site.register(Report, ReportAdmin)
-admin.site.register(Price, ReportAdmin)
+admin.site.register(Price, PriceAdmin)
